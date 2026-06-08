85a7d509435e65185d9014d6ee625d48bd596a4e
서하루 <seoharo0111@gmail.com>
2026-06-08 16:54:08 +0900

    vite error edit

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // 커스텀 도메인(function-factory.kro.kr)을 사용하므로 base 경로는 '/'로 설정
-  base: '/',
+  base: '/function-factory-club-site/',
 });
